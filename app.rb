6113378ed6433bb5d54acc6206fa9db4d2e158ef
minling <mzhao725@gmail.com>
2015-06-26 23:03:11 -0400

    done

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'config/environment'
 class GameApp < Sinatra::Base
   get '/rps/:play' do
     # Your app code goes here
+
+  
+     @rpsgame = RPSGame.new(params[:play].to_sym)
+
+
     erb :rps_game
+    #take the viewer/user to the file called rps_game that lives in the views directory
   end
 end
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,2 +1,101 @@
 class RPSGame
+
+  attr_accessor :computer_play, :choice
+
+  class PlayTypeError < StandardError
+  end
+
+  WINNINGMATCHES ={
+    :rock => :scissors,
+    :paper => :rock,
+    :scissors => :paper
+  }
+
+  TIEDMATCHES ={
+    :rock => :rock,
+    :paper => :paper,
+    :scissors => :scissors
+  }
+
+  LOSTMATCHES ={
+    :rock => :paper,
+    :paper => :scissors,
+    :scissors => :rock
+  }
+
+  def initialize(choice)
+    if !(choice == :rock || choice == :paper || choice == :scissors)
+       raise PlayTypeError
+    end
+    @choice = choice 
+    computer_turn 
+  end
+
+  def self.valid_play?(choice)
+    if choice == :rock 
+      true
+    elsif choice == :paper 
+      true
+    elsif choice == :scissors
+      true
+    else 
+      false
+    end
+  end
+
+  def computer_play
+    @computer_play
+  end
+
+  def computer_turn
+    @computer_play = [:rock, :paper, :scissors].sample
+  end
+
+  def won?
+    @computer_play = computer_play
+    computer = @computer_play
+    player = @choice
+ 
+    if WINNINGMATCHES[player] == computer 
+      true
+    else
+      false
+    end 
+  end
+
+  def tied?
+    @computer_play = computer_play
+    computer = @computer_play
+    player = @choice
+ 
+    if TIEDMATCHES[player] == computer 
+      true
+    else
+      false
+    end 
+  end
+
+  def lost?
+    @computer_play = computer_play
+    computer = @computer_play
+    player = @choice
+ 
+    if LOSTMATCHES[player] == computer 
+      true
+    else
+      false
+    end 
+  end
+
+  def result
+  
+    if won?
+      "You won!"
+    elsif tied?
+      "You are tied!"
+    elsif lost?
+      "You lost!"
+    end
+  end 
 end
+
